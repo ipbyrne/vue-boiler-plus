ca48f0149398ee2ef24e43983b6b48967dab7d68
Isaac Byrne <ipbyrne@me.com>
2019-11-07 13:26:38 -0600

    Moving wordpress URL to config file

--- client/src/js/client_config.js
+++ client/src/js/client_config.js
@@ -1,4 +1,5 @@
 module.exports =  {
   base_url: window.location.origin == "http://localhost:8080" || window.location.origin == "HOSTED DEV URL" ? "http://localhost:5000" : "https://www.tracingdelta.com",
+  wordpress_url: 'https://public-api.wordpress.com/rest/v1.1/sites/tracingdeltablog.home.blog',
   posts_per_page: 12
 }
